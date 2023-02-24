d013380d35fa8d3db8c4e6282469c16c225db99b
brayan suarez <57419379+brayan2003@users.noreply.github.com>
2023-02-23 19:01:40 -0500

    metodo de suma

--- src/co/edu/poli/ces3/Estudiantes.java
+++ src/co/edu/poli/ces3/Estudiantes.java
@@ -2,6 +2,9 @@ package co.edu.poli.ces3;
 
 public class Estudiantes {
     public static void main(String[] args) {
-
+        System.out.println("Hola mundo");
+    }
+    public static int suma (int a, int b){
+        return a+b;
     }
 }
